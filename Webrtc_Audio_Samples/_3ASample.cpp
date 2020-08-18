238c8f698b631d7a3bfa56a0d696d183ffa94aa6
lina <>
2020-08-18 21:53:33 +0800

    fix compile error

---
+++ Webrtc_Audio_Samples/_3ASample.cpp
@@ -0,0 +1,75 @@
+#include "_3ASample.h"
+#include "rtc_base/logging.h"
+#include "absl/strings/string_view.h"
+#include "rtc_base/ref_counted_object.h"
+
+#include <iostream>
+
+
+using namespace std;
+
+
+_3ASample::_3ASample()
+{
+	rtc::LogMessage::LogToDebug(rtc::LoggingSeverity::LS_INFO);
+	rtc::LogMessage::SetLogToStderr(true);
+
+	webrtc::Config conig;
+	m_audioMixer = webrtc::AudioMixerImpl::Create();
+	m_apm = new rtc::RefCountedObject<webrtc::AudioProcessingImpl>(conig);
+	if (m_apm->Initialize() != webrtc::AudioProcessing::kNoError) {
+		delete m_apm;
+		m_apm = nullptr;
+	}
+
+	m_audioTransport = new webrtc::AudioTransportImpl(m_audioMixer, m_apm);
+	m_audioDevice = webrtc::CreateAudioDeviceWithDataObserver(
+		webrtc::AudioDeviceModule::AudioLayer::kPlatformDefaultAudio,
+		nullptr);
+}
+
+_3ASample::~_3ASample()
+{
+}
+
+int _3ASample::init()
+{
+	//log
+
+
+	if (m_audioDevice == nullptr) {
+		cout << "audio device = nullptr";
+		return -1;
+	}
+	int32_t ret = m_audioDevice->Init();
+
+	if (ret < 0) {
+		cout << "init failed" << endl;
+		return -1;
+	}
+
+	if (m_audioDevice->RecordingDevices() > 0) {
+		m_audioDevice->SetRecordingDevice(0);
+	}
+	
+	ret = m_audioDevice->InitRecording();
+	if (ret < 0) {
+		cout << "InitRecording failed" << endl;
+		return -1;
+	}
+
+	ret = m_audioDevice->InitMicrophone();
+	if (ret < 0) {
+		cout << "InitMicrophone failed" << endl;
+		return -1;
+	}
+
+	m_audioDevice->RegisterAudioCallback(m_audioTransport);
+	m_audioDevice->StartRecording();
+	return 0;
+}
+
+bool _3ASample::setRecordingDevice(const char deviceId[MAX_DEVICE_ID_LEN])
+{
+	return false;
+}
---
+++ Webrtc_Audio_Samples/_3ASample.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define MAX_DEVICE_ID_LEN 512
+
+#include "modules/audio_device/win/audio_device_core_win.h"
+#include "modules/audio_device/audio_device_buffer.h"
+#include "rtc_base/task_queue_win.h"
+#include "audio/audio_transport_impl.h"
+#include "modules/audio_device/include/audio_device_data_observer.h"
+#include "modules/audio_mixer/audio_mixer_impl.h"
+#include "modules/audio_processing/audio_processing_impl.h"
+
+class _3ASample
+{
+public:
+	_3ASample();
+	~_3ASample();
+
+	int init();
+	bool setRecordingDevice(const char deviceId[MAX_DEVICE_ID_LEN]);
+
+private:
+	rtc::scoped_refptr<webrtc::AudioDeviceModule> m_audioDevice = nullptr;
+	webrtc::AudioTransportImpl	*m_audioTransport = nullptr;
+
+	rtc::scoped_refptr<webrtc::AudioMixerImpl> m_audioMixer;
+	webrtc::AudioProcessingImpl*	m_apm;
+};
+
